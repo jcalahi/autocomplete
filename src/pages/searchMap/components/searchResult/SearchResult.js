83a5a093c05d6fe2410278e5d2bb48cad400e508
Julius <julius@dealtale.com>
2022-07-28 18:46:28 +0800

    removed inline styles

--- src/pages/searchMap/components/searchResult/SearchResult.js
+++ src/pages/searchMap/components/searchResult/SearchResult.js
@@ -2,10 +2,11 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { connect } from 'react-redux';
 import Marker from './components/marker';
+import './styles.css';
 
 const SearchResult = ({ coordinates }) => {
   return (
-    <div style={{ height: '100vh' }}>
+    <div className="container">
       <GoogleMapReact
         bootstrapURLKeys={process.env.REACT_APP_GOOGLE_API_KEY}
         defaultZoom={16}
